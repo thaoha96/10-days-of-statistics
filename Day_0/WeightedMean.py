0b840fabf103c30b502c732eaca416f44b615d0f
thaoha96 <thaoha.mephi.ru@gmail.com>
2021-12-19 00:44:27 +0700

    Update WeightedMean.py

--- Day_0/WeightedMean.py
+++ Day_0/WeightedMean.py
@@ -1,3 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'weightedMean' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY X
+#  2. INTEGER_ARRAY W
+#
 def weightedMean(X, W):
     # Write your code here
     sums = 0
